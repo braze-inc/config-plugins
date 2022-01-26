fee2a123e124f8c2be295e64c3e8b477710082d2
Giau. Tran Minh <giau.tmg@gmail.com>
2022-01-26 15:25:42 +0700

    chore: update example app

--- apps/app/src/react-native-fbsdk-next/App.tsx
+++ apps/app/src/react-native-fbsdk-next/App.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import { SafeAreaView, Text, StyleSheet } from "react-native";
-import { LoginButton, Settings } from "react-native-fbsdk-next";
+import React, { useCallback } from "react";
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableHighlight,
+} from "react-native";
+import { LoginButton, ShareDialog } from "react-native-fbsdk-next";
+import type { ShareContent } from "react-native-fbsdk-next";
 
+const SHARE_LINK_CONTENT: ShareContent = {
+  contentType: "link",
+  contentUrl: "https://www.facebook.com/",
+};
 
 export default function App() {
+  const shareLinkWithShareDialog = useCallback(async () => {
+    const canShow = await ShareDialog.canShow(SHARE_LINK_CONTENT);
+    if (canShow) {
+      try {
+        const { isCancelled, postId } = await ShareDialog.show(
+          SHARE_LINK_CONTENT
+        );
+        if (isCancelled) {
+          Alert.alert("Share cancelled");
+        } else {
+          Alert.alert("Share success with postId: " + postId);
+        }
+      } catch (error) {
+        Alert.alert("Share fail with error: " + error);
+      }
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
-      <LoginButton/>
+      <LoginButton
+        testID="facebook-login"
+        onLoginFinished={(error, data) => {
+          Alert.alert(JSON.stringify(error || data, null, 2));
+        }}
+      />
+      <TouchableHighlight onPress={shareLinkWithShareDialog}>
+        <Text style={styles.shareText}>Share link with ShareDialog</Text>
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -16,5 +53,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  shareText: {
+    fontSize: 20,
+    margin: 10,
   },
 });
