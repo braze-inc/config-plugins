bd0ee811fec6a7e865cd13e879c9553c96fb11a6
Giau. Tran Minh <giau.tmg@gmail.com>
2022-02-26 02:37:16 +0700

    chore: initial workspace for testing google-signin

---
+++ packages/google-signin/src/withGoogleSignin.ts
@@ -0,0 +1,25 @@
+import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
+
+/**
+ * Apply google-signin configuration for Expo SDK 42 projects.
+ */
+const withGoogleSignin: ConfigPlugin<{} | void> = (config, _props = {}) => {
+  // Support passing no props to the plugin.
+  const props = _props || {};
+
+  // Return the modified config.
+  return config;
+};
+
+const pkg = {
+  // Prevent this plugin from being run more than once.
+  // This pattern enables users to safely migrate off of this
+  // out-of-tree `@config-plugins/google-signin` to a future
+  // upstream plugin in `google-signin`
+  name: "google-signin",
+  // Indicates that this plugin is dangerously linked to a module,
+  // and might not work with the latest version of that module.
+  version: "UNVERSIONED",
+};
+
+export default createRunOncePlugin(withGoogleSignin, pkg.name, pkg.version);
